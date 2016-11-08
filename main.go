f3b3cfc4bf4864c063dccbdb0262fbd6d4d8c52b
Alex Egger <alex.egger96@gmail.com>
2016-11-08 21:27:36 +0100

    Add command parsing

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/base64"
 	"flag"
 	"log"
 	"os"
 	"path"
-	"path/filepath"
+	"strings"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
@@ -20,28 +18,42 @@ const (
 	DatabaseFile     = "index.db"
 )
 
-var restoreFlag = flag.Bool("restore", false, "Show a hidden file.")
-
 func main() {
 	flag.Parse()
 	InitEnvironment()
 
-	db, err := InitDB()
+	_, err := InitDB()
 	if err != nil {
-		log.Fatalf("%v\n", err)
+		log.Panicf("%v\n", err)
 	}
 
-	log.Printf("File: %v\n", flag.Arg(0))
-	if *restoreFlag {
-		if err := Restore(flag.Arg(0)); err != nil {
-			log.Fatalf("%v\n", err)
+	if len(flag.Args()) < 2 {
+		flag.Usage()
+	}
+
+	source := flag.Arg(0)
+	switch strings.ToLower(flag.Arg(0)) {
+	case "release":
+		source = "/home/alex/.local/share/hidden"
+		target := flag.Arg(1)
+		destination := "."
+
+		if len(flag.Args()) >= 3 {
+			destination = flag.Arg(2)
 		}
-	} else {
-		if err := Hide(db, flag.Arg(0)); err != nil {
-			log.Fatalf("%v\n", err)
+		if err := stash.Release(source, target, destination); err != nil {
+			log.Panicf("%v\n", err)
+		}
+	case "stash":
+		source = flag.Arg(1)
+		fallthrough
+	default:
+		if err := stash.Stash(source, "/home/alex/.local/share/hidden"); err != nil {
+			log.Panicf("%v\n", err)
 		}
 	}
 }
+
 func InitEnvironment() {
 	path := path.Join(getHome(), DataStoragePath)
 	if _, err := os.Stat(path); os.IsNotExist(err) {
@@ -63,52 +75,26 @@ func InitDB() (db *gorm.DB, err error) {
 	return db, nil
 }
 
-func Hide(db *gorm.DB, root string) (err error) {
-	// Compute the filename for the archive.
-	name := path.Join(getDataPath(), computeFilename(root))
-	name += ArchiveExtension
-
-	// Create the file for the archive.
-	file, err := os.Create(name)
-	if err != nil {
-		return err
-	}
-
-	if err := stash.Pack(root, file); err != nil {
-		return err
-	}
-
-	// Add entry to the database.
-	absPath, err := filepath.Abs(root)
-	if err != nil {
-		return err
-	}
-
-	entry := stash.Entry{Path: absPath}
-	db.Create(&entry)
-	return nil
-}
-
-func Restore(path string) (err error) {
-	name := filepath.Join(getDataPath(), computeFilename(path))
-	name += ArchiveExtension
-
-	file, err := os.Open(name)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	path, err = filepath.Abs(path)
-	if err != nil {
-		return err
-	}
-
-	if err := stash.Unpack(filepath.Dir(path), file); err != nil {
-		return err
-	}
-	return nil
-}
+//func Restore(path string) (err error) {
+//	name := filepath.Join(getDataPath(), computeFilename(path))
+//	name += ArchiveExtension
+//
+//	file, err := os.Open(name)
+//	if err != nil {
+//		return err
+//	}
+//	defer file.Close()
+//
+//	path, err = filepath.Abs(path)
+//	if err != nil {
+//		return err
+//	}
+//
+//	if err := stash.Unpack(filepath.Dir(path), file); err != nil {
+//		return err
+//	}
+//	return nil
+//}
 
 func getHome() string {
 	homePath := os.Getenv("HOME")
@@ -120,10 +106,4 @@ func getHome() string {
 
 func getDataPath() string {
 	return path.Join(getHome(), DataStoragePath)
-}
-
-func computeFilename(path string) string {
-	hasher := sha1.New()
-	hasher.Write([]byte(path))
-	return base64.URLEncoding.EncodeToString(hasher.Sum(nil))
 }
